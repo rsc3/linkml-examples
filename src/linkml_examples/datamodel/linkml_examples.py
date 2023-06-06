65f9f723ceff6932d6de0ddec257bc10b7c7a346
rsc3 <sidcox@gmail.com>
2023-06-06 09:54:44 -0700

    chore: initial commit

--- src/linkml_examples/datamodel/linkml_examples.py
+++ src/linkml_examples/datamodel/linkml_examples.py
@@ -1,5 +1,5 @@
 # Auto generated from linkml_examples.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-06-06T09:31:38
+# Generation date: 2023-06-06T09:54:41
 # Schema: linkml-examples
 #
 # id: https://w3id.org/my-org/linkml-examples
